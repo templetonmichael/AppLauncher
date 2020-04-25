0323333aaf7df2fd4a8f54d664cc12c36f97134f
Michael Templeton <38446638+templetonmichael@users.noreply.github.com>
2020-04-25 14:46:26 -0500

    Initial Commit

---
+++ AppLauncher/AppLauncher/Applications.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace AppLauncher
+{
+    public static class Applications
+    {
+        private const string Slack = "C:\\Users\\Michael.Templeton\\AppData\\Local\\slack\\slack.exe";
+        private const string Outlook = "C:\\Program Files\\Microsoft Office\\Office16\\OUTLOOK.EXE";
+
+        public static List<string> AppList = new List<string>()
+        {
+            Slack,
+            Outlook
+        };
+    }
+}
---
+++ AppLauncher/AppLauncher/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mime;
+using System.IO;
+
+namespace AppLauncher
+{
+    class Controller
+    {
+        static void Main(string[] args)
+        {
+            List<Process> processes = new List<Process>();
+            try
+            {
+                foreach (var app in Applications.AppList)
+                {
+                    var process = new Process {StartInfo = {FileName = app}};
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
